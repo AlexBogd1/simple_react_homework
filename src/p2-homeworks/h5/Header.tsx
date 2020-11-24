af08246e06ddbfcd5a436946a5b56a255813c9c1
Alex <7894664@mail.ru>
2020-11-24 20:42:20 +0300

    add routers

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
+import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+    const [openNavbar, setOpenNavbar] = useState<boolean>(true)
 
+    const onClickHandler = () => {
+        setOpenNavbar(!openNavbar)
+    }
+
+    return (
+        <div className={s.header} onClick={onClickHandler}>
+            {openNavbar &&
+            <ul className={s.ul}>
+                <li className={s.li}><NavLink activeClassName={s.active} to="/pre-junior">Pre Junior</NavLink></li>
+                <li className={s.li}><NavLink activeClassName={s.active} to="/junior">Junior</NavLink></li>
+                <li className={s.li}><NavLink activeClassName={s.active} to="/junior-plus">Junior Plus</NavLink></li>
+            </ul>}
         </div>
     );
 }
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-
+import s from './Error404.module.css'
 function Error404() {
     return (
-        <div>
-            <div>404</div>
-            <div>Page not found!</div>
-            <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+        <div className={s.notfound}>
+            <div className={s.centered}><span className={s.inverted}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>&nbsp;
+            </div>
+            <div className={s.centered}><span className={s.inverted}>&nbsp;4&nbsp;0&nbsp;4&nbsp;</span><span
+                className={s.shadow}>&nbsp;</span></div>
+            <div className={s.centered}><span className={s.inverted}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span><span
+                className={s.shadow}>&nbsp;</span></div>
+            <div className={s.centered}>&nbsp;<span className={s.shadow}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+            </div>
+            <div className={s.row}>&nbsp;</div>
+            <div className={s.row}>A fatal exception 404 has occurred at C0DE:ABAD1DEA in 0xC0DEBA5E.</div>
+            <div className={s.row}>The current request will be terminated.</div>
+            <div className={s.row}>&nbsp;</div>
+            <div className={s.row}>* Press any key to return to the previous page.</div>
+            <div className={s.row}>* Press CTRL+ALT+DEL to restart your computer. You will</div>
+            <div className={s.row}>&nbsp;&nbsp;lose any unsaved information in all applications.</div>
+            <div className={s.row}>&nbsp;</div>
+            <div className={s.centered}>Press any key to continue...<span className={s.blink}>&#9608;</span></div>
         </div>
     );
 }
