5fe7faa2331f6d9a12c5a05cdecd83cc845b8b8a
Alex <7894664@mail.ru>
2020-11-24 18:33:27 +0300

    Updates

---
+++ precache-manifest.83e599bd109525d609aff1fd4a802f1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed367a4584f5e1fb89d8c0ec5ab83529",
+    "url": "/simple_react_homework/index.html"
+  },
+  {
+    "revision": "eec8f59b9489c35f3245",
+    "url": "/simple_react_homework/static/css/main.3b27fd7a.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/simple_react_homework/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple_react_homework/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec8f59b9489c35f3245",
+    "url": "/simple_react_homework/static/js/main.e4109028.chunk.js"
+  },
+  {
+    "revision": "e46c9f9ef852752a3901",
+    "url": "/simple_react_homework/static/js/runtime-main.8954a448.js"
+  }
+]);
