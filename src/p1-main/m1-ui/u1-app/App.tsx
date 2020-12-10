b5edb7e86e0e105175e8b2e86577580dac451fa5
Alex <7894664@mail.ru>
2020-12-11 00:27:22 +0300

    complete 7-th task

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./App.module.css";
 import HW6 from "../../../p2-homeworks/h6/HW6";
 import HW7 from "../../../p2-homeworks/h7/HW7";
+import HW5 from "../../../p2-homeworks/h5/HW5";
 
 function App() {
     return (
@@ -11,9 +12,9 @@ function App() {
             {/*<HW2/>*/}
             {/*<HW3/>*/}
             {/*<HW4/>*/}
-            {/*<HW5/>*/}
+            <HW5/>
             {/*<HW6/>*/}
-                <HW7/>
+            {/*    <HW7/>*/}
         </div>
     );
 }
